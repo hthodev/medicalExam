6f0071b3b8076489fb345f73657300708d489442
Nguyen Van Dat <dat.nv@neo-lab.vn>
2022-11-26 13:00:28 +0700

    BOOKING-10 move api url

--- tests/controller/users.test.js
+++ tests/controller/users.test.js
@@ -1,6 +1,7 @@
 const chai = require('chai');
 const request = require('supertest');
 const { server } = require('../../src/server');
+const { baseurl } = require('../base');
 const db = require('../../src/models/index');
 
 const { expect } = chai;
@@ -44,50 +45,59 @@ describe('Register & Login user', () => {
   }
 
   it('Should error email when login', async () => {
-    const { body, status } = await request('http://localhost:3000/api/')
+    const { body, status } = await request(baseurl)
                                       .post('register')
                                       .send(fail_params)
-    expect(body.result.message).to.equal("email invaild")
-    expect(status).to.equal(200);
+    expect(body.result.message).equal("email invaild")
+    expect(status).equal(200);
   })
 
   it('Should register successfully', async () => {
-    const { body, status } = await request('http://localhost:3000/api/')
+    const { body, status } = await request(baseurl)
                                       .post('register')
                                       .send(true_params)
-    expect(body.result.message).to.equal("registry successfully")
-    expect(status).to.equal(200);
+    expect(body.result.message).equal("registry successfully")
+    expect(status).equal(200);
   })
 
   it('Should error email when login', async () => {
-    const { body, status } = await request('http://localhost:3000/api/')
+    const { body, status } = await request(baseurl)
                                       .post('login')
                                       .send({ 'email': 'nvdatdev', 'password': '123123' })
-    expect(body.message.message).to.equal("email isn't registered")
-    expect(status).to.equal(200);
+    expect(body.message.message).equal("email isn't registered")
+    expect(status).equal(200);
   })
 
-  it('Should when login with no password', async () => {
-    const { body, status } = await request('http://localhost:3000/api/')
+  it('Should error no password when login', async () => {
+    const { body, status } = await request(baseurl)
                                       .post('login')
                                       .send({ 'email': 'nvdatdev@gmail.com', 'password': '' })
-    expect(status).to.equal(500);
-    expect(body.message).to.equal("Missing inputs parameter!")
+    expect(status).equal(500);
+    expect(body.message).equal("Missing inputs parameter!")
   })
 
-  it('Should when login with error password', async () => {
-    const { body, status } = await request('http://localhost:3000/api/')
+  it('Should error password when login', async () => {
+    const { body, status } = await request(baseurl)
                                       .post('login')
                                       .send({ 'email': 'nvdatdev@gmail.com', 'password': '123' })
-    expect(status).to.equal(200);
-    expect(body.message.message).to.equal("password wrong")
+    expect(status).equal(200);
+    expect(body.message.message).equal("password wrong")
   })
 
   it('Should login successfully', async () => {
-    const { body, status } = await request('http://localhost:3000/api/')
+    const { body, status } = await request(baseurl)
                                       .post('login')
                                       .send({ 'email': 'nvdatdev@gmail.com', 'password': '123123' })
-    expect(body.message.message).to.equal('login successfully')
-    expect(status).to.equal(200);
+    expect(body.message.message).equal('login successfully')
+    expect(status).equal(200);
+  })
+
+  it('Should get list users successfully', async () => {
+    const { body, status } = await request(baseurl)
+                                      .get('get-user')
+    expect(body.result.length).equal(2);
+    expect(body.result[0].email).equal('nvdatdev1@gmail.com');
+    expect(body.result[1].email).equal('nvdatdev@gmail.com');
+    expect(status).equal(200);
   })
 })
